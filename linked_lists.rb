98c6afaed2b66e3e826113e370aed7cb7b4be152
Reece Bradley <naruto73@hotmail.co.uk>
2023-03-10 13:42:11 +0000

    add prepend method and print_list method

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,9 +1,41 @@
 class Node
-  attr_reader :value
-  attr_accessor :next_node
+  attr_accessor :value, :next_node
+
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
 end
 
+class LinkedList
+  #attr_accessor :head
+
+  def initialize(value)
+    @head = Node.new(value)
+  end
+
+  def append(value)
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(value)
+  end
+
+  def prepend(value)
+    if @head == nil
+      current_node.next_node = Node.new(value)
+    else
+      @head = Node.new(value, @head)
+    end
+  end
+
+  def print_list
+    current_node = @head
+    puts current_node.value
+    while (current_node = current_node.next_node)
+      puts current_node.value
+    end
+  end
+
+end
---
+++ test.rb
@@ -0,0 +1,10 @@
+require_relative 'linked_lists.rb'
+
+bob = LinkedList.new(1)
+bob.append(2)
+bob.append(3)
+bob.prepend(5)
+bob.append(4)
+bob.prepend(6)
+bob.print_list
+
